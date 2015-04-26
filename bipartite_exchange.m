11228bf226f18d44d29406dbc98effd323ffc93c
Craig Robert Shenton <c.shenton@surrey.ac.uk>
2015-04-26 15:07:18 +0100

    add code

---
+++ bipartite_exchange.m
@@ -0,0 +1,17 @@
+
+% B = n buyers
+% S = m sellers
+
+% B_{i} has 1 cash
+% S_{j} has 1 wheat
+
+% A = bipartite adjacency matrix
+% a_{ij} = 1 if B_{i} can trade with S_{j}
+
+% u^{b} = (u^{b}_{1}, u^{b}_{2},...,u^{b}_{n}) buyer utility functions
+% u^{s} = (u^{s}_{1}, u^{s}_{2},...,u^{s}_{n}) seller utility functions
+
+% x^{s}_{j} = seller exchange rate (price) in cash per unit wheat
+% x^{b}_{i} = buyer exchange rate (price) in wheat per unit cash
+
+% w_{ij} = amount of wheat from i to j (link weight)
