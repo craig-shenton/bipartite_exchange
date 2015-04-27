758b4c03ba6c6459f69dbcaa3332ad5288ff85fc
Craig Robert Shenton <c.shenton@surrey.ac.uk>
2015-04-27 14:34:17 +0100

    algorithm

--- bipartite_exchange.m
+++ bipartite_exchange.m
@@ -19,24 +19,91 @@
 %% graph adjacency matrix %%
 
 % A = -------------
-%     | a_11 a_12 |
-%     | a_21 a_22 |
+%     | 0    a_12 |
+%     | a_21 0    |
 %     -------------
-
-A = [0 1; 1 0]; 
+%
 % gives two nodes buyer i and seller j connected via edges a_{12} and a_{21}
+
+A = [0 1; 1 0];
+
+n = length(A); 
 
 %% endownments %%
 
-% vectors = [i j]
+% vectors = agents [i j]
 
 wheat = [0 1]; % amount of wheat at start of turn
 
 cash = [1 0]; % amount of cash at start of turn
 
-utility_wheat = [1 0];
+utility_wheat = [1 0]; % willing to exhange wheat for n units of cash
 
-utility_cash = [0 1];
+utility_cash = [0 1]; % willing to exhange cash for n units of wheat
+
+%% trade algorithm %%
+
+for i=1
+
+% i looks at nearest-neghbours in A, a_ij for all j
+    
+i_neghbours = A(i,:);
+
+% find i's nearest-neghbours offer prices
+
+i_neghbours_price = utility_wheat.*i_neghbours;
+
+% find j that is offering the min price for wheat (min(utility_wheat)) > 0
+
+[min_price,min_id] = min(i_neghbours_price(i_neghbours_price > 0));
+
+% is min_price <= utility_cash(i) (i.e., is j willing to trade)
+% if so, j trades min_price*cash(i) for min_price*wheat(i)
+
+wheat_1 = []; % initialise wheat stocks after trade
+cash_1 = [];  % initialise cash stocks after trade
+
+if min_price <= utility_cash(i)
+   % new wheat stocks (buyer) = old wheat stocks + amount purchased
+   wheat_1(min_id) = wheat(min_id)+(min_price*wheat(i));
+   % new wheat stocks (seller) = old wheat stocks - amount sold
+   wheat_1(i) = wheat(i)-(min_price*cash(min_id));
+   % new cash stocks (buyer) = old cash stocks - amount paid
+   cash_1(min_id) = cash(min_id)-min_price*wheat(i);
+   % new cash stocks (seller) = old cash stocks + amount profit
+   cash_1(i) = cash(i)+(min_price*cash(min_id));
+else
+   wheat_1 = wheat 
+   cash_1 = cash;
+end
+
+end
+
+P = []; % price matrix at end of turn
+
+% P = -------------
+%     | 0    p_12 |
+%     | p_21 0    |
+%     -------------
+%
+% p_ij = the price at which j sold wheat to i
 
 X = []; % trade matrix at end of turn
+
+% X = -------------
+%     | 0    x_12 |
+%     | x_21 0    |
+%     -------------
+%
+% x_ij = the amount of seller j's wheat that buyer i consumes
+
+
+
+
+
+
+
+
+
+
 
